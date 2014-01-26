045731f8d0669aecad314de6112e6d7f83644824
qur2 <aurelien.scoubeau@gmail.com>
2014-01-26 13:35:27 +0100

    Compute pair comparisons in a triangular matrix.

--- lib/score.ex
+++ lib/score.ex
@@ -31,9 +31,58 @@ defmodule Score do
 		t ++ h
 	end
 
+	def sign(n) when n > 0 do 1 end
+	def sign(n) when n < 0 do -1 end
+	def sign(n) when n == 0 do 0 end
+	def range_sum(n, i) do
+		# sum(n-i..n)
+		# sum(0..n) - sum(0..n-i)
+		Float.floor(n*(n-1)/2 - (n-i)*(n-i-1)/2)
+	end
+
+	@doc """
+	Given a ranking, returns a comparison triangle matrix where every element
+	is compared to every other.
+	"""
+	def pairs(ranks) do
+		len = length ranks
+		graph = 1..(len*2) |> Enum.map(fn(_) -> 0 end)
+		_pairs(ranks, len, 0, graph)
+		# |> Enum.map(fn({{s, t}, weight}) ->
+		# 	if weight > 0 do
+		# 		{{s, t}, weight}
+		# 	else
+		# 		{{t, s}, -weight}
+		# 	end
+		# end)
+	end
+	defp _pairs(_, n, i, scores) when n == i do
+		scores
+	end
+	defp _pairs(arr, n, i, acc) when n > i do
+		[h | t] = arr
+		a = Enum.with_index(t) |> Enum.reduce(acc, fn {x, j}, acc ->
+			inc = sign(h-x)
+			IO.puts "n=#{n}, i=#{i}, j=#{j}, index=#{range_sum(n, i)+j}"
+			if inc != 0 do
+				# index for upper triangle matrix
+				arc_update(acc, range_sum(n, i)+j, sign(x-h))
+				# for the record, index in the lower triangle matrix works as:
+				# range_sum(n-1, j)+i-1
+			else
+				acc
+			end
+		end)
+		_pairs(t, n, i+1, a)
+	end
+	def arc_update(graph, i, inc) do
+		{k, [h|t]} = Enum.split(graph, i)
+		k ++ [h+inc|t]
+	end
+
 	@doc """
 	Adds two element together. When applied to lists, it builds a new list
-	by zipping the lists and adding each pair member.
+	by zipping the lists and adding each pair of members.
 	"""
 	def add(a, b) when is_integer(a) and is_integer(b) do
 		a + b
