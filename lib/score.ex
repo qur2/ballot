d6483c52ae16c4733efb3041bd886414a906223d
qur2 <aurelien.scoubeau@gmail.com>
2014-01-31 09:27:29 +0100

    Rename stuff a bit better.

--- lib/score.ex
+++ lib/score.ex
@@ -5,32 +5,8 @@ defmodule Score do
 	"""
 
 	@doc """
-	Given a ranking, returns a comparison matrix where every element is compared
-	to every other.
+	Returns the sign of the number, or 0 if number == 0.
 	"""
-	def count(arr) do
-		_count(arr, length(arr), 0, [])
-	end
-	defp _count(_, n, i, counts) when n == i do
-		counts
-	end
-	defp _count(arr, n, i, counts) when n > i do
-		# Head is the current element, tail contains the rest.
-		[h | t] = arr
-		# Compare head to every other element.
-		# First elem is [0] for the elem against himself.
-		# Ties count for 0.
-		c = [0] ++ Enum.map(t, fn(x) -> if h < x do 1 else 0 end end)
-		# Recursively call the function on the tail to which we append the head
-		# in order to simulate a circular list.
-		# Result needs to be shifted accordingly.
-		_count(t ++ [h], n, i+1, counts ++ [rotate(c, n-i)])
-	end
-	def rotate(arr, pivot) do
-		{h, t} = Enum.split(arr, pivot)
-		t ++ h
-	end
-
 	def sign(n) when n > 0 do 1 end
 	def sign(n) when n < 0 do -1 end
 	def sign(n) when n == 0 do 0 end
@@ -41,12 +17,12 @@ defmodule Score do
 	end
 
 	@doc """
-	Given a ranking, returns a comparison triangle matrix where every
+	Given a ballot, returns a comparison triangle matrix where every
 	element is compared to every other.
 	"""
-	def pairs(ranks) do
-		len = length ranks
-		_pairs(ranks, len, 0, [])
+	def pairs(ballot) do
+		len = length ballot
+		_pairs(ballot, len, 0, [])
 	end
 	# to n-1 because the last call would be done with a single value anyway
 	defp _pairs([h | t], n, i, acc) when i < n-1 do
@@ -74,41 +50,43 @@ defmodule Score do
 	@doc """
 	Sums an array of comparison matrices.
 	"""
-	def sum(counts) do
-		Enum.reduce(counts, fn(count, acc) -> add(count, acc) end)
+	def sum(cmp_mat) do
+		Enum.reduce(cmp_mat, fn(count, acc) -> add(count, acc) end)
 	end
 
-	def schwartz_graph(counts) do
-		[a_count | the_rest] = counts
-		n = length(a_count) - 1
+	@doc """
+	Builds a graph using the provided comparison matrices.
+	"""
+	def graph(ballots) do
+		n = length(Enum.at(ballots, 0)) - 1
 		keys = 0..(n-1) |> Enum.flat_map(fn i ->
 			(i+1)..n |> Enum.map(fn j -> {i, j} end)
 		end)
-		values = counts |> Enum.map(fn c -> pairs(c) end) |> sum
-		graph = Enum.zip(keys, values) |> Enum.reduce(HashDict.new, fn {{i, j}, v}, g ->
+		values = ballots |> Enum.map(fn b -> pairs(b) end) |> sum
+		#Build the graph from keys and values + reverse negative arcs
+		Enum.zip(keys, values) |> Enum.reduce(HashDict.new, fn {{i, j}, v}, g ->
 			if v < 0 do
 				HashDict.put(g, {j, i}, -v)
 			else
 				HashDict.put(g, {i, j}, v)
 			end
 		end)
-		graph
 	end
 
-	def elect(graph) do
+	def schwartz_set(graph) do
 		# find nodes that are never a destination
-		{o, d} = Enum.reduce(graph, {HashSet.new, HashSet.new}, fn {{i, j}, v}, {o, d} ->
+		{o, d} = Enum.reduce(graph, {HashSet.new, HashSet.new}, fn {{i, j}, _}, {o, d} ->
 			{HashSet.put(o, i), HashSet.put(d, j)}
 		end)
 		diff = HashSet.difference(o, d)
 		if HashSet.size(diff) > 0 do
 			diff
 		else
-			elect(reduce_graph(graph))
+			schwartz_set(reduce_graph(graph))
 		end
 	end
 	def reduce_graph(graph) do
-		{kmin, vmin} = Enum.reduce(graph, fn {{i, j}, v}, {{imin, jmin}, vmin} ->
+		{kmin, _} = Enum.reduce(graph, fn {{i, j}, v}, {{imin, jmin}, vmin} ->
 			if v < vmin do
 				{{i, j}, v}
 			else
@@ -119,7 +97,7 @@ defmodule Score do
 	end
 
 	def clone(l, n) do
-		Enum.map(0..(n-1), fn x -> l end)
+		Enum.map(0..(n-1), fn _ -> l end)
 	end
 	def wikipedia() do
 		#   01234
@@ -138,6 +116,6 @@ defmodule Score do
 		++ clone([1, 3, 0, 4, 2], 7)
 		++ clone([2, 1, 0, 3, 4], 2)
 		++ clone([4, 3, 1, 0, 2], 7)
-		++ clone([2, 1, 4, 3, 0], 8) |> schwartz_graph |> elect
+		++ clone([2, 1, 4, 3, 0], 8) |> graph |> schwartz_set
 	end
 end
